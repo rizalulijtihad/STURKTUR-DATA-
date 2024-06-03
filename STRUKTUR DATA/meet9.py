85219c879278bb4abba0cafdc9e6e539a1c6f34b
rizalulijtihad <146051176+rizalulijtihad@users.noreply.github.com>
2024-06-03 11:47:42 +0800

    struktur data 3 juni 2024

---
+++ STRUKTUR DATA/meet9.py
@@ -0,0 +1,22 @@
+# percabangan jika maka
+
+name = input("what is your name")
+# if kondisi : aksi
+
+if name == "rizal" : print("mahasiswa")
+print("end of program")
+#print("end of program")
+
+#if indentasi
+if name=="edy":
+   print("mahasiswa")
+   print("mahasiswa")
+print("end of program")
+
+# 3. IF ELSE
+if name=="rizal" :
+    print("mahasiswa")
+    print(f"terimakasih{name}")
+else:
+    print("anda bukan mahasiswa")
+print("end of program")
